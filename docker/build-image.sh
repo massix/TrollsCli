da819b4b0a55bf6e373bed797ff1b4b48c3c2119
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2017-10-29 15:54:35 +0100

    Add docker things and stuff.

---
+++ docker/build-image.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+REGISTRY="registry.massi.rocks:80/"
+DOCKER_IMAGE="trolls/cli"
+DOCKER_TAG="latest"
+
+cp ../build/libs/TrollsController-1.0.0-SNAPSHOT.jar .
+docker build -t ${REGISTRY}${DOCKER_IMAGE}:${DOCKER_TAG} .
+docker push ${REGISTRY}${DOCKER_IMAGE}:${DOCKER_TAG}
---
+++ docker/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar /usr/lib/TrollsController-1.0.0-SNAPSHOT.jar $*
