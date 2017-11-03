581e0f2fcc11f5c1f480fbd0850fd0d127b589c5
Massimo GENGARELLI <mgengarelli.ext@orange.com>
2017-11-03 11:53:56 +0100

    Better docker generation if proxy is needed.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -5,5 +5,5 @@ DOCKER_IMAGE="trolls/cli"
 DOCKER_TAG="latest"
 
 cp ../build/libs/TrollsController-1.0.0-SNAPSHOT.jar .
-docker build -t ${REGISTRY}${DOCKER_IMAGE}:${DOCKER_TAG} .
+docker build --network=host "$@" -t ${REGISTRY}${DOCKER_IMAGE}:${DOCKER_TAG} . && \
 docker push ${REGISTRY}${DOCKER_IMAGE}:${DOCKER_TAG}
